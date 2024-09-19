261eb97fab164561ea6b92056162415f449e127a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-19 15:22:28 +0300

    commonjs module example

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import path from 'path';
-import fs from 'fs'; //ES import 
-
-fs.writeFileSync('log.txt', 'hello files');
-let name = path.basename('C:\\temp\\myfile.html');
-console.log(name);
+const calc = require('./calc.js');
+calc.add(4,5);
