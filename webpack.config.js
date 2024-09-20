66fd5baf88c894170dfdd5bdc64e450be5da4dc8
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-20 10:49:25 +0300

    add server and css

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', "css-loader"],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             //title: 'Kaspari äpp',
