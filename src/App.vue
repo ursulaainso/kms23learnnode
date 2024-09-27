10c5b72623d5cb79fa2e999eb26c2fd20393fa8b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-27 09:40:52 +0300

    Simple item list example

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script setup>
 import { ref } from "vue";
-let message = ref('Hello Vue');
-setTimeout(() => {
-    message.value = 'Hello Kaspar';
-}, 10000);
+let message = ref('');
+let items = ref([
+    'piim',
+    'viin',
+    'sai',
+    'leib'
+]);
+
+function addItem() {
+    if(message.value.trim() !== ''){
+        items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
 </script>
 
 <template>
     <div class="container">
-        <div class="content">
-            <h1>{{ message }}</h1>
-            <button class="button is-primary">Click me</button>
+        <div class="content mt-3">
+
+            <div class="field has-addons">
+                <div class="control">
+                    <input v-model="message" @keydown.enter="addItem" class="input" type="text" placeholder="Item to add">
+                </div>
+                <div class="control">
+                    <button class="button is-info" @click="addItem">
+                        Add
+                    </button>
+                </div>
+            </div>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
         </div>
     </div>
 </template>
-
-
