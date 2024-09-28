51d770b3d358d3d0cae09fa54786235d5e006a4f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-28 09:47:12 +0300

    Add ImgModal make modal usable with other elements

---
+++ src/components/ImgModal.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Modal from './Modal.vue';
+defineProps(['isOpen', 'url']);
+
+</script>
+<template>
+    <Modal :isOpen="isOpen" @close="$emit('close')">
+        <p class="image is-4by3">
+            <img :src="url" alt="">
+        </p>
+    </Modal>
+</template>
